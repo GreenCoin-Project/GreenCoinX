74a81b1ded9c974eb304d2bcf69078aa82caf3d2
Nilam Doctor <nilamdoc@gmail.com>
2015-08-27 09:01:13 -0500

    Press

--- app/views/greencoin/press.html.php
+++ app/views/greencoin/press.html.php
@@ -2,6 +2,15 @@
 <div style="margin:20px;padding:20px;">
 <div style="background-color:white;padding:10px"><h3>Press Release</h3></div>
 <div style="background-color:white;padding:10px">
+
+<blockquote>
+<h3>GREENCOINX ADDRESSES BLOCK CHAIN MINING LONGEVITY</h3>
+<h5>DIGITAL FOUNDATION ESTABLISHED TO PROVIDE LONG TERM MINING REWARDS</h5>
+<p><small>Toronto, Ontario, August 27, 2015</small>GreenCoinX Inc, the developer of the world’s first identifiable crypto currency and which has XGC as its digital currency identifier, announces that it has addressed the problem of long term crypto currency mining rewards.<a href="/files/GreenCoinX press release Digital Foundation & Longevity August 27  2015.pdf" target="_blank">read more...</a></p>
+</blockquote>
+
+
+
 <blockquote>
 <h3>GREENCOINX FORMS ADVISORY BOARD</h3>
 <h5>GreenCoinX to Receive Benefit of Talented Brain Trust</h5>
