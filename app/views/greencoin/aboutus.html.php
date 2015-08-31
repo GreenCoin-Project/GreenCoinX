7dec07a2546fd525390c78047c27ab7cf17b0c57
Nilam Doctor <nilamdoc@gmail.com>
2015-08-31 10:17:21 -0500

    about us

--- app/views/greencoin/aboutus.html.php
+++ app/views/greencoin/aboutus.html.php
@@ -20,6 +20,90 @@
 <p>To use XGC,  please use the free XGC Online Wallet available at <a href="https://xgcwallet.org" target="_blank">www.xgcwallet.org</a></p>
 
 <p>To use XGC on a mobile, please download the XGC Mobile wallet at <a href="https://play.google.com/store/apps/details?id=org.xgcwallet.mobile" target="_blank">https://play.google.com/store/apps/details?id=org.xgcwallet.mobile</a></p>
+<div class="row" style="padding:20px">
+	<h3>Our Leadership</h3>
+	<h4>Directors and Management</h4>
+	<blockquote>
+	<p><strong>Daniel Wettreich is the Chairman & CEO of GreenCoinX.</strong>  He is a director and CEO of GreenBank Capital Inc a merchant bank publicly listed on the Canadian Securities Exchange. He is also a director  of five other CSE listed companies.  He has more than 40 years of experience in venture capital, private equity, and management of publicly traded companies.   He has been Chairman and CEO of Churchill Venture Capital LP, a Dallas, Texas private equity business, for more than 20 years. He has been a director of public companies listed on NASDAQ, the American Stock Exchange, the London Stock Exchange, the AIM Market of the London Stock Exchange, and the Vancouver Stock Exchange, a predecessor to the TSX Venture Exchange.  These public companies have been in diverse businesses in internet technologies, oil and gas, retailing, telecommunications, media, and real estate.   He has facilitated 13 reverse takeover transactions. He is a graduate of the University of Westminster with a BA in Business.  </p>
+	</blockquote>
+	
+	<blockquote>
+	<p><strong>Nilam Doctor is the President & Chief Technical Officer of GreenCoinX.</strong> He is the lead developer of GreenCoinX. Previously he was Owner of Hitarth Consultants a software consultancy firm focused on web based projects and digital currency. He was a Grant Committee Member of the Bitcoin Foundation promoting the Bitcoin protocol. He was also CEO of Real Bitcoins, an Indian based promoter of crypto currency. He has been a project manager and lead software developer for online educational companies, Learn without Limits, MobyMax, and Safal Education. He graduated with a MBA in Finance and a Bachelor of Engineering from Gujarat University.
+	</p>
+	</blockquote>
+	
+	<blockquote>
+	<p><strong>Paul Cullingham is a director of GreenCoinX.</strong> He is a director and Vice President of GreenBank Capital Inc, a Canadian merchant bank listed on the Canadian Securities Exchange. He is also Chief Compliance Officer if its investment banking subsidiary GreenBank Financial Inc. He is a director of five publicly listed mining exploration companies. He is CEO of Ubique Minerals Inc, a private mining exploration company, and of Inside Bay Street Corporation, an online portal for Canadian investors. He has been an investment banker for 29 years specializing in the resource and financial sectors.</p>
+	</blockquote>
+	
+	<blockquote>
+	<p><strong>David M. Lonsdale is a director of GreenCoinX.</strong> He is the President and CEO of The Lonsdale Group, a Dallas-based private investor in small cap companies. He is a director of GreenBank Capital Inc a Canadian merchant bank listed on the Canadian Securities Exchange. Previously he was for ten years the President of Allegiance Capital Corporation, a private investment bank focusing on mergers and acquisitions, with offices in Dallas, New York, and Chicago. Mr. Lonsdale has successfully built and sold three venture-funded information technology companies, including selling one of them to Microsoft. Earlier in his career he managed corporate divisions of McDonnell Douglas/Boeing and Dun & Bradstreet/A C Nielsen. He obtained his MBA in Finance & Marketing from Cornell University and his B.Sc. in Physics & Mathematics from Leeds Beckett University in the U.K.</p>
+	</blockquote>
+	<hr>
+	<h4>Advisory Board</h4>	
+	
+	<blockquote>
+	<p><strong>Rob Levy is Founder of LIG Ventures</strong>, a Silicon Valley based investment and consulting firm, and Executive in Residence with PricewaterhouseCoopers (PwC), the worlds’ second largest professional services business with offices in 157 countries and revenues of $34B. Previously he was Chief Products Officer of Progress Software (NASDAQ:PRGS), an enterprise application development and deployment software company. He was Chief Technology Officer of BEA Systems (NASDAQ:BEAS, sold to Oracle in 2008 for $8.5B), an enterprise software company with revenues in excess of $1.5B, and Chief Technology Strategist of Computer Associates (NASDAQ:CA), which is one of the largest software companies in the world. He has previously been a director of Revelytix Inc, (sold to Teradata), an enterprise data company, CEO of Eyeona a software company marketing a retail pricing application, Managing Director US Operations of ItemField, a venture backed startup (sold to Informatica), and Senior Vice President of Nastel Technologies, an enterprise software company.
+	</p>
+	</blockquote>
+	
+	<blockquote>
+	<p><strong>David J. Robino is a private investor</strong> and corporate and nonprofit governance expert.  He was previously a non-executive director of Insight Enterprises, Inc., (NASDAQ: NSIT) a Fortune 500 global provider of information technology, and a non-executive director of Memec, Inc. then the world’s largest distributor of specialty electronics and semiconductors which was sold to Avnet, Inc., (NYSE: AVT).  Before those board roles, Mr. Robino served as the Vice Chairman of Gateway, Inc.  His earlier career was spent at The Maytag Corporation, Pepsi-Cola, AC Nielsen, and AT&T.  He has a B.A. from Graceland University and an M.S. from Iowa State University.  He currently teaches Business Ethics at Southern Oregon University.
+	</p>
+	</blockquote>
+
+	<blockquote>
+	<p><strong>Nigel Stuart Little is the former Group Vice Chairman of Canaccord Capital Corporation</strong>, one of the largest investment banking businesses in Canada.  He was President of Canaccord Adams, the European division of Canaccord, for seven years.  Previously he headed up the international divisions at Nomura International, Morgan Stanley International, NationsBanc Panmure/WestLB and HSBC James Capel.  Since retiring from Canaccord, he has been a Senior Advisor to Kleinwort Benson Bank, and is currently a Senior Advisor to PPR Estates, a London based private real estate investment company. He has been a Director or Chairman of a number of public and private companies involved in technology, finance, natural resources, and Real Estate.  He graduated with an Honors degree in Natural Sciences from the University of London and is a Fellow of The Securities Institute and a Freeman of the City of London.
+	</p>
+	</blockquote>
+ 
+	<blockquote>
+	<p><strong>Kevin Kuykendall is a serial entrepreneur and is currently CEO of Vivione Biosciences Inc</strong>, a Canadian public company commercializing a bacteria testing technology. He is also CEO and Founder of Tripad LLC, a manufacturer of travel and photography accessories, and Chairman of Health2o Products LLC, a nutraceutical beverage and cosmeceutical skincare products business. Previously he was CEO and Founder of White Energy Company LLC, an ethanol producer.
+	</p>
+	</blockquote>
+
+	<blockquote>
+	<p><strong>Dwayne Walker is Senior VP at Rightside Group</strong>, a Nasdaq listed technology and domain services company. He has been a director, investor and executive at several technology and internet companies including iClick, Escrow.com, FreeShop.com, Micro General, US Connect, Network Commerce, DomainSponsor and Artemis Ventures. He was President of Nasdaq listed Fidelity National Information Solutions, a provider of banking, real estate and payment technologies. Previously he was Director of Sales and Marketing for Microsoft.
+	</p>
+	</blockquote>
+
+	<blockquote>
+	<p><strong>Bob Heckman is President of The Allen Group</strong>, a management consulting firm focused on business development and marketing. Previously he was Director of Business Development at Accenture (formerly Anderson Consulting), and he has held senior management positions at MTech and IBM. He is the author of Boardroom Selling: Proven Strategies for Selling to Decision Makers. He is a seasoned international  speaker at corporate sales meetings and industry conferences, and is a specialist at executive leadership coaching.
+	</p>
+	</blockquote>
+
+	<blockquote>
+	<p><strong>Johann Tse is CEO of Aquarian Capital</strong>, an advisory firm which specializes in strategic opportunities in China and with Chinese businesses overseas, from M&A advisory to developing renewable power plants, agribusinesses and natural resources as principals, with offices in Dallas, Beijing, Shanghai, Hong Kong and Tel Aviv. Previously he was Director for International M&A for Yum! Brands, and founded corporate venture capital program for Rohm and Haas Company. He held senior management positions with Swire Group, a UK/Hong Kong conglomerate, where he established 13 companies across numerous industries and was responsible for government relations and corporate development for China. He was the founding Deputy Chairman of the Hong Kong Chamber of Commerce in Shanghai, and was an Executive Director of the Hong Kong Chamber of Commerce in China. He is currently board member of Texas-Israel Chamber of Commerce, Dallas Business Club, and Executive Board Member of Circle Ten Council, Boy Scouts of America. He has an MBA from INSEAD, France, and a Bachelor of Science (Hons.) from the Chinese University of Hong Kong in Electronics Engineering.
+	</p>
+	</blockquote>
+
+	<blockquote>
+	<p><strong>Alan Molloy is Chairman of Hillberry Trust Company Limited</strong>, a Fiduciary company based in the Isle of Man. He provides independent director and consultancy services, and sits on the Investment Committee of a UK investment management company, having previously been the managing director of the international services of Duncan Lawrie, a British private bank. He is a Fellow of the Institute of Banking in Ireland, a Chartered Fellow of the UK Chartered Institute for Securities and Investments and is a Chartered Wealth Manager. He is also a published author on investment related matters.
+	</p>
+	</blockquote>
+
+	<blockquote>
+	<p><strong>Vinnie Grosso is a telecommunications and entertainment industry veteran</strong> having held senior posts with NBC, AT&T, PRIMEDIA, and as CEO of start-up Into Networks. He is an established entrepreneur with “disruptive” technology and business models for streaming video, interactive television, video-on-demand, software-on-demand and mobile commerce for more than two decades. As President of "AT&T Downtown Digital"  he led the first interactive TV trials for AT&T, headed up NBC's efforts with TV affiliates on the Internet, led the technology effort for Primedia to distribute educational software by satellite and built the largest USA and Canadian distributor of broadband games for the cable industry at Into Networks. He is also recognized as a board member of The National Association of Television Program Executives, working the early business models for digital entertainment. He is currently a consultant to Fortune 500 Companies and start-ups alike.
+	</p>
+	</blockquote>
+
+	<blockquote>
+	<p><strong>Trevor Koverko is the CEO of Polymath Computer Solutions Inc</strong>, a virtual reality real estate marketing business. Previously he was CEO of eProf Education Inc a technology startup connecting educators and students in a virtual classroom. He is an early adopter of crypto currency with a diversified angel investor portfolio. He is retired from the New York Rangers where he was a draft pick in 2005, and previously played professional hockey in the Ontario Hockey League. He is a graduate from the University of Western Ontario-Richard Ivey School of Business.
+	</p>
+	</blockquote>
+
+	<blockquote>
+	<p><strong>Jim Taylor is Senior Solution Architect for Liaison Technologies Inc</strong>, a data integration and data security company. He was Chief Technology Officer of bTrade Inc a venture-funded software company focused on providing a secure platform for internet-based business transactions, which was purchased by Click Commerce. He previously was head of systems programming teams at Pier 1 Imports and Levi Strauss.
+	</p>
+	</blockquote>
+ 
+	<blockquote>
+	<p><strong>Joe Devich is a Partner at Newport Board Group</strong>, a national professional management services firm. He was previously Founder of TipApplause, a developer of mobile phone applications, and President of Knowberry, a web based utility focused on public safety. He was also President of three other technology companies, BizTube, Mosaix, and Asta Networks. He has held senior management positions with Covad Communications, Qwest Communications, Ameritech, and EDS.
+	</p>
+	</blockquote>
+
+ 	
+</div>
 
 </div>
 </div>
