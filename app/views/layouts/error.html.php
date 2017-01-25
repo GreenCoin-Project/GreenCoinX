6e56e2eaa80f003417673ec7d47dd6282ac48b6b
Nilam Doctor <nilamdoc@gmail.com>
2017-01-25 17:39:17 +0530

    error

--- app/views/layouts/error.html.php
+++ app/views/layouts/error.html.php
@@ -1,73 +1,272 @@
-<?php
-/**
- * Lithium: the most rad php framework
- *
- * @copyright     Copyright 2013, Union of RAD (http://union-of-rad.org)
- * @license       http://opensource.org/licenses/bsd-license.php The BSD License
- */
-
-/**
- * This layout is used to render error pages in both development and production. It is recommended
- * that you maintain a separate, simplified layout for rendering errors that does not involve any
- * complex logic or dynamic data, which could potentially trigger recursive errors.
- */
-use lithium\core\Libraries;
-$path = Libraries::get(true, 'path');
-?>
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-	<?php echo $this->html->charset(); ?>
-	<title>Unhandled exception</title>
-	<?php echo $this->html->style(array('bootstrap.min', 'lithified', 'debug')); ?>
-	<?php echo $this->scripts(); ?>
-	<?php echo $this->styles(); ?>
-	<?php echo $this->html->link('Icon', null, array('type' => 'icon')); ?>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="">	
+	<meta name="description" content="">		
+	<meta name="author" content="">
+	<link rel="shortcut icon" href="/img/logo-GreenCoinX.gif" />
+	<title>GreenCoinX: Identified Digital Currency </title>
+	<!-- Bootstrap Core CSS -->
+	<link href="/bootstrap/css/bootstrap.css" rel="stylesheet">
+	
+	
+	<!-- Custom CSS -->
+		
+	<link href="/bootstrap/css/grayscale.css" rel="stylesheet">
+		<!-- Custom styles for this template -->
+	
+				
+	<!-- Custom Fonts -->
+	<link href="/fontawesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+	<script src="/js/jquery.js"></script>
+ <script src="/bootstrap/js/bootstrap-datepicker.js"></script>	
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	<![endif]-->
 </head>
-<body class="lithified">
-	<div class="container">
-		<div class="masthead">
-			<ul class="nav nav-pills pull-right">
-				<li>
-					<a href="http://li3.me/docs/manual/quickstart">Quickstart</a>
-				</li>
-				<li>
-					<a href="http://li3.me/docs/manual">Manual</a>
-				</li>
-				<li>
-					<a href="http://li3.me/docs/lithium">API</a>
-				</li>
-				<li>
-					<a href="http://li3.me/">More</a>
-				</li>
-			</ul>
-			<a href="http://li3.me/"><h3>&#10177;</h3></a>
-		</div>
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top"  onLoad="UpdateDetails();" >
+							<!-- Navigation -->
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="/en/">
+																 <i class="fa fa-play-circle"></i>  <span class="light">GreenCoin</span><span style="color:red">X</span> 
+                </a>
+																<span style="padding:10px;" class="pull-right">
+																<a class=" page-scroll" href="#" onclick="ChangeLanguage('en','/');" style="border-bottom:double"><small>English</small></a>	
+																<a class=" page-scroll" href="#" onclick="ChangeLanguage('de','/');" style="border-bottom:double"><small>Deutsch</small></a>
+																<a class=" page-scroll" href="#" onclick="ChangeLanguage('hi','/');" style="border-bottom:double"><small>हिन्दी</small></a>
+																<!--
+<a href="#" onclick="ChangeLanguage('es','/');" style="border-bottom:double"><small>Español</small></a>
 
-		<hr>
+																-->
+																</span>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+																				
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+												
+        </div>
+        <!-- /.container -->
 
-		<div class="row-fluid">
-			<h1>An unhandled exception was thrown</h1>
-			<h3>Configuration</h3>
-			<p>
-				This layout can be changed by modifying
-				<code><?="{$path}/views/layouts/error.html.php";?></code>
-			</p>
-			<p>
-				To modify your error-handling configuration, see
-				<code><?="{$path}/config/bootstrap/errors.php";?></code>
-			</p>
-		</div>
+    </nav>
 
-		<div class="content">
-			<?php echo $this->content(); ?>
-		</div>
+    <!-- Intro Header -->
+    <header class="intro">
+        <div class="intro-body">
+            <div class="container">
+                <div class="row">
+																				<div class="col-lg-6 col-lg-offset-3">
+																				<p>&nbsp;</p>
+																					<img  src="/img/logo-GreenCoinX.png" style="max-width:100%"/>
+																				</div>
+																			
+                    <div class="col-md-12">
+                        <h1 class="brand-heading">THE WORLD&#039;S FIRST CRYPTO CURRENCY REQUIRING USER IDENTIFICATION</h1>
+                        <p class="intro-text">
+																								 THE PROMISE OF CRYPTO CURRENCY WAS ALWAYS THE FAST AND INEXPENSIVE TRANSFER OF FUNDS WORLDWIDE, WHILE BYPASSING THE BANKING SYSTEM.</p> 
+																									<p class="intro-text">BEFORE GREENCOINX THIS WAS IMPEDED BY IDENTITY CONCERNS.</p>
+																									<p class="intro-text">NOW GREENCOINX HAS MADE THIS PROMISE A REALITY.</p>
+																			</div>
+																		<div class="col-md-12" style="text-align:centre;background-color:#efe;margin-top:5px">
+																				<ul class="nav navbar-nav" style="font-family:Verdana; font-size:13px">
+																					<li><a href="/en/greencoin/contact">contact</a></li>		
+																					<li><a href="/en/greencoin/aboutus">about us</a></li>	
+																					<li><a href="/en/greencoin/risk">risk</a></li>				
+																					<li><a href="/en/greencoin/privacy">privacy</a></li>		
+														<!--							<li><a href="/greencoin/how">how it works?</a></li>		-->
+																					<li><a href="/en/greencoin/FAQ">faq</a></li>				
+<!--																					<li><a href="/blockchain">block chain</a></li>-->
+																					<li><a href="/en/greencoin/download">open a new wallet</a></li>
+																					<li><a href="https://xgcwallet.org" target="_blank">XGC Wallet</a></li>
+																					<li><a href="https://kycglobal.net" target="_blank">KYC Global</a></li>
+																					<li><a href="https://siicrypto.com" target="_blank">SiiCrypto</a></li>
+																					<li><a href="/en/greencoin/government">government</a></li>
+																					<li><a href="/en/greencoin/press">press</a></li>
+																					<li><a href="/en/greencoin/identification">identification</a></li>
+																				</ul>
+																				</div>
+																				<div class="col-md-8 col-md-offset-2">
+																				 <a href="#about" class="btn btn-circle page-scroll">
+                         <i class="fa fa-angle-double-down animated"></i>
+                        </a>
+</div>
+                </div>
+            </div>
+        </div>
+    </header>
 
-		<hr>
+    <!-- About Section -->
+    <section id="about" class="content-section text-center">
+        <div class="row">
+            <div class="col-lg-10 col-lg-offset-1">
+													<div class="row">
+														<div class="col-lg-6 col-lg-offset-3">
+															<img  src="/img/logo-GreenCoinX.png" style="max-width:100%"/>
+														</div>
+													</div>
+													<h3 style="color:white">ALL GREENCOINX USERS HAVE TO BE FULLY IDENTIFIED BEFORE USAGE,</h3><h3 style="color:white"> SO BAD ACTORS WILL NOT USE GREENCOINX</h3>
+													<p style="text-align:justify;font-size:16px;color:black;margin-top:-20px"></p>
+												<p style="text-align:justify;font-size:16px;color:black;margin-top:-20px">In October, 2014 Bill Gates, the founder of Microsoft, when asked about bitcoin stated;													<blockquote style="text-align:justify;padding-left:20px;margin-top:-20px;color:white">&quot;The customers we&#039;re talking about aren&#039;t trying to be anonymous, they&#039;re willing to be known, so bitcoin technology is key and you can add to it or you could build a similar technology where there&#039;s enough attribution where people feel comfortable that this is nothing to do with terrorism or any type of money laundering.&quot; 
+													<footer style="padding:0px;margin:0px;color:white"><a  style="color:white" href="http://www.bloomberg.com/video/bill-gates-bitcoin-is-exciting-because-it-s-cheap-dQ4qHV4~TLSnUIuIRfZBVA.html" target="_blank">Bill Gates, Bloomberg TV</a></footer>
+													</blockquote>
+												</p>
+												<h3 style="color:white">THE GREENCOINX ECOSYSTEM COMPRISES THE WORLD&#039;S FIRST CRYPTO CURRENCY REQUIRING USER IDENTIFICATION,</h3>
+												<h3 style="color:white">A FREE ONLINE WALLET, AND </h3>
+												<h3 style="color:white">A FREE ONLINE CRYPTO CURRENCY EXCHANGE.</h3>
+												</p>
+												<p style="text-align:center;font-size:16px;color:white;margin-top:-20px">TO OPEN YOUR FREE ONLINE GREENCOINX WALLET GO TO <a  style="color:white" href="https://XGCWALLET.ORG">WWW.XGCWALLET.ORG</a></p>
+																<a href="#features" class="btn btn-circle page-scroll">
+                 <i class="fa fa-angle-double-down animated"></i>
+                </a>
+																
+            </div>
+        </div>
+    </section>
+<div class="col-md-12" style="text-align:centre;background-color:#efe;margin-top:5px">
+																				<ul class="nav navbar-nav" style="font-family:Verdana; font-size:13px">
+																					<li><a href="/en/greencoin/contact">contact</a></li>
+																					<li><a href="/en/greencoin/aboutus">about us</a></li>	
+																					<li><a href="/en/greencoin/risk">risk</a></li>				
+																					<li><a href="/en/greencoin/privacy">privacy</a></li>		
+														<!--							<li><a href="/en/greencoin/how">how it works?</a></li>		-->
+																					<li><a href="/en/greencoin/FAQ">faq</a></li>				
+<!--																					<li><a href="/en/blockchain">block chain</a></li>-->
+																					<li><a href="/en/greencoin/download">open a new wallet</a></li>
+																					<li><a href="https://xgcwallet.org" target="_blank">XGC Wallet</a></li>
+																					<li><a href="https://kycglobal.net" target="_blank">KYC Global</a></li>
+																					<li><a href="https://siicrypto.com" target="_blank">SiiCrypto</a></li>
+																					
+																					<li><a href="/en/greencoin/government">government</a></li>
+																					<li><a href="/en/greencoin/press">press</a></li>
+																					<li><a href="/en/greencoin/identification">identification</a></li>
+																				</ul>
+																				</div>
 
-		<div class="footer">
-			<p>&copy; Union Of RAD 2013</p>
-		</div>
+    <!-- Feature Section -->
+    <section id="features" class="content-section text-center">
+        <div class="row">
+            <div class="col-lg-10 col-lg-offset-1">
+													<div class="row">
+														<div class="col-lg-6 col-lg-offset-3"><br>
+															<img  src="/img/logo-GreenCoinX.png" style="max-width:100%"/>
+														</div>
+													</div><br>
+
+                <h3 style="color:black">Crypto currency is the most significant technological breakthrough to impact the financial world since the Internet.																</h3>
+																<p>&nbsp;</p>
+																<p style="color:black">GreenCoinX is a medium of exchange created and stored electronically, which uses encryption techniques to control the creation of monetary units and verify the transfer of funds. Due to encryption , transactions are secure from third parties. GreenCoinX is anti-inflationary as its value is not affected by government money printing. GreenCoinX allows users access to a fast execution anytime/anywhere low cost global payment system which bypasses the banking system.</p>
+																
+                <h3 style="color:DeepSkyBlue"> GREENCOINX IS THE ACCEPTABLE ALTERNATIVE CRYPTO CURRENCY TO FIAT CURRENCIES</h3>
+<h3  style="color:DeepSkyBlue">GreenCoinX is the Anti-Bitcoin. It has solved all the Bitcoin flaws. The GreenCoinX Answer is... </h3>
+																
+																 <ul style="font-size:20px;color:DeepSkyBlue"><li>User Identification</li>
+<li>Currency Stability</li>
+
+<li>Blockchain Longevity</li>
+
+<li>Free and Safe Crypto Currency Exchange</li>
+
+<li>Government Partnerships</li>
+</ul>
+ <h3 style="color:black"><a href="#">The User Attraction:</a> Funds can be transferred at a fraction of the cost and speed of the banking system. Micro payments can be easily completed. The 1.5 Billion unbanked people in the world can transfer funds simply with a mobile phone. Online retailers can substantially increase their market size by accessing the unbanked. Payment intermediaries can significantly improve cost/speed of their transactions.</h3>
+
+<h3 style="color:white">JOIN THE CRYPTO CURRENCY REVOLUTION !  OPEN A GREENCOINX WALLET NOW</h3>
+
+<h4 style="color:black"><a href="#">GreenCoinX Inc</a> is a subsidiary of <a href="#">GreenBank Capital Inc</a> a Canadian public company (<a href="#">CSE:GBC and OTCMKTS:GRNBF</a>)</h4>
+
+<h4 style="color:black">For more information on GreenBank please see <a href="http://www.GreenBankCapitalInc.com" target="_blank">www.GreenBankCapitalInc.com</a></h4>
+
+																
+            </div>
+        </div>
+    </section>
+<div class="col-md-12" style="text-align:centre;background-color:#efe;margin-top:5px">
+																				<ul class="nav navbar-nav" style="font-family:Verdana; font-size:13px">
+																					<li><a href="/en/greencoin/contact">contact</a></li>		
+																					<li><a href="/en/greencoin/aboutus">about us</a></li>	
+																					<li><a href="/en/greencoin/risk">risk</a></li>				
+																					<li><a href="/en/greencoin/privacy">privacy</a></li>		
+														<!--							<li><a href="/en/greencoin/how">how it works?</a></li>		-->
+																					<li><a href="/en/greencoin/FAQ">faq</a></li>				
+																					<!--																					<li><a href="/en/blockchain">block chain</a></li>-->
+																					<li><a href="/en/greencoin/download">open a new wallet</a></li>
+																					<li><a href="https://xgcwallet.org" target="_blank">XGC Wallet</a></li>
+																					<li><a href="https://kycglobal.net" target="_blank">KYC Global</a></li>
+																					<li><a href="https://siicrypto.com" target="_blank">SiiCrypto</a></li>
+																					
+																					<li><a href="/en/greencoin/government">government</a></li>
+																					<li><a href="/en/greencoin/press">press</a></li>
+																					<li><a href="/en/greencoin/identification">identification</a></li>
+																				</ul>
+																				</div>    <!-- Signup Section -->
+	
+											
+								<div class="view 				">
+																		</div>
+									<!-- Footer -->
+<footer>
+	<div class="container text-center ">
+		<small><span class="pull-right">185.65 ms</span></small>
 	</div>
-</body>
+</footer>
+	
+	
+
+	
+						
+	<script src="/js/main.js?v=6157555"></script>
+	
+		<!-- jQuery -->
+		
+		<!-- Bootstrap Core JavaScript -->
+		<script src="/bootstrap/js/bootstrap.min.js"></script>
+		<!-- Plugin JavaScript -->
+		<script src="/js/jquery.easing.min.js"></script>
+		<!-- Custom Theme JavaScript -->
+		<script src="/js/grayscale.js"></script>
+	</body>
 </html>
+<script type="text/javascript">
+$(function() {
+ $('.tooltip-x').tooltip();
+ $("input:text:visible:first").focus();
+});
+</script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-58995548-1', 'auto');
+  ga('send', 'pageview');
+</script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-66175124-1', 'auto');
+  ga('send', 'pageview');
+</script>
