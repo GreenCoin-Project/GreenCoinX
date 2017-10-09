94971681cef3db5f514eb6a39db3e8a83c84094a
Nilam Doctor <nilamdoc@gmail.com>
2017-10-09 12:46:02 +0530

    ]

--- app/views/elements/footer.html.php
+++ app/views/elements/footer.html.php
@@ -1,7 +1,7 @@
 <!-- Footer -->
 <footer>
 	<div class="container text-center ">
-		<small><span class="pull-right"><?php print_r($_SERVER["REMOTE_ADDR");?> - <?php echo number_format($pagetotaltime*1000,2);  ?> ms</span></small>
+		<small><span class="pull-right"><?php print_r($_SERVER["REMOTE_ADDR"]);?> - <?php echo number_format($pagetotaltime*1000,2);  ?> ms</span></small>
 	</div>
 </footer>
 	
